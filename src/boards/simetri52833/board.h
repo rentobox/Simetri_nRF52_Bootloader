46380b6fcd5cb363a273d5645f679c854dadeccd
Renato Gutierrez <rgutierrez@simetri.us>
2025-06-14 08:24:31 -0400

    Push to my version

--- src/boards/simetri52833/board.h
+++ src/boards/simetri52833/board.h
@@ -22,43 +22,42 @@
  * THE SOFTWARE.
  */
 
-#ifndef _ARDUINOBLE33_NRF52840_H
-#define _ARDUINOBLE33_NRF52840_H
-
-#define _PINNUM(port, pin)    ((port)*32 + (pin))
+#ifndef SIMETRI52833_H
+#define SIMETRI52833_H
 
 /*------------------------------------------------------------------*/
 /* LED
  *------------------------------------------------------------------*/
-#define LEDS_NUMBER           1  // red LED
-#define LED_PRIMARY_PIN       _PINNUM(0, 24)
-#define LED_STATE_ON          0
+#define LEDS_NUMBER         2
+#define LED_PRIMARY_PIN     13
+#define LED_SECONDARY_PIN   14
+#define LED_STATE_ON        0
 
 /*------------------------------------------------------------------*/
 /* BUTTON
  *------------------------------------------------------------------*/
-#define BUTTONS_NUMBER        2
-#define BUTTON_1              _PINNUM(1, 11)  // D2 switch
-#define BUTTON_2              _PINNUM(1, 12)  // D3 switch
-#define BUTTON_PULL           NRF_GPIO_PIN_PULLUP
+#define BUTTONS_NUMBER      2
+#define BUTTON_1            11
+#define BUTTON_2            12
+#define BUTTON_PULL         NRF_GPIO_PIN_PULLUP
 
 //--------------------------------------------------------------------+
 // BLE OTA
 //--------------------------------------------------------------------+
-#define BLEDIS_MANUFACTURER   "Arduino"
-#define BLEDIS_MODEL          "Nano 33 BLE"
+#define BLEDIS_MANUFACTURER    "Nordic"
+#define BLEDIS_MODEL           "PCA10100"
 
 //--------------------------------------------------------------------+
 // USB
 //--------------------------------------------------------------------+
 #define USB_DESC_VID           0x239A
-#define USB_DESC_UF2_PID       0x0063
-#define USB_DESC_CDC_ONLY_PID  0x0064
+#define USB_DESC_UF2_PID       0x00D8
+#define USB_DESC_CDC_ONLY_PID  0x00D8
+#define USB_DESC_PRODUCT       "Simetri nRF52833 DK"
 
-//------------- UF2 -------------//
-#define UF2_PRODUCT_NAME   "Arduino Nano 33 BLE"
-#define UF2_VOLUME_LABEL   "NANO33BOOT"
-#define UF2_BOARD_ID       "nRF52840-Nano-33"
-#define UF2_INDEX_URL      "https://www.arduino.cc/"
+#define UF2_PRODUCT_NAME       "SIMETRI Inc."
+#define UF2_VOLUME_LABEL       "SMRI833BOOT"
+#define UF2_BOARD_ID           "nRF52833-SIMETRI-revA"
+#define UF2_INDEX_URL          "https://simetri.us"
 
-#endif // _ITSY_NRF52840_H
+#endif // SIMETRI52833_H
